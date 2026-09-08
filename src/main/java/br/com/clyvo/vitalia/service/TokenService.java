ecbeb3f30b1d406ae66f43cece5a35ff7ef3f578
manuelalacerda <manuelalacerda88@gmail.com>
2026-09-08 14:06:10 -0300

    fix: ajusta expiração do JWT, configuração de testes com H2 e validação do contexto

--- src/main/java/br/com/clyvo/vitalia/service/TokenService.java
+++ src/main/java/br/com/clyvo/vitalia/service/TokenService.java
@@ -8,6 +8,7 @@ import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
@@ -48,6 +49,6 @@ public class TokenService {
     }
 
     private Instant genExpirationDate() {
-        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+        return Instant.now().plus(Duration.ofHours(2));
     }
 }
