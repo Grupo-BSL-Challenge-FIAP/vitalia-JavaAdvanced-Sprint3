0a68c4385631a5f9c0e1d9edf9841423345c8c04
manuelalacerda <manuelalacerda88@gmail.com>
2026-09-11 17:28:15 -0300

    correção: remove o campo de senha do contrato AppUserRequest no endpoint de atualização

--- src/main/java/br/com/clyvo/vitalia/dto/request/AppUserRequest.java
+++ src/main/java/br/com/clyvo/vitalia/dto/request/AppUserRequest.java
@@ -7,6 +7,5 @@ import jakarta.validation.constraints.Size;
 public record AppUserRequest(
         @NotBlank @Size(max = 150) String fullName,
         @NotBlank @Email @Size(max = 150) String email,
-        @NotBlank @Size(min = 6, max = 100) String password,
         @Size(max = 30) String phone
 ) {}
